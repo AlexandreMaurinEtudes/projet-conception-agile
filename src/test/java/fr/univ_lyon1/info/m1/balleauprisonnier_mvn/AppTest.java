34f696eb8d828bca626a49e6fd9b15124ebd990c
Lachand-Pascal <valentin@lachand.net>
2022-09-15 14:59:17 +0200

    Initial commit

---
+++ src/test/java/fr/univ_lyon1/info/m1/balleauprisonnier_mvn/AppTest.java
@@ -0,0 +1,38 @@
+package fr.univ_lyon1.info.m1.balleauprisonnier_mvn;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
